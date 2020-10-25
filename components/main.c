ba1283cb8bbdcd3b297b4a12dfd30b3420f3ce5c
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-10-24 23:54:48 -0300

    Uses git-description-parser in the main funcion

--- components/main.c
+++ components/main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include "strings/inc/strings.h"
+#include "git-description-parser/git_describe_parser.h"
 #include "math/mathematics.h"
 #include "dep_header.h"
 
+#define SECTION_SEPARATOR "====================================\n"
+
 int main(void)
 {
+    //Testing git description parser
+    git_description_t my_description = git_describe_parse();
+
+    printf("Commit hash: %s\n", my_description.commit_hash_short);
+    printf("Raw description: %s\n", my_description.raw_description);
+    printf("Version numbers: ");
+    for (int counter = 0; counter < my_description.used_version_numbers; counter++) {
+        printf("%i.", my_description.version_numbers[counter]);
+    }
+    printf("\b \n");
+    printf("There are %i commits after the last tag\n", my_description.commits_after_tag);
+    if (my_description.release_candidate_number < 0) {
+        printf("It's a release!\n");
+    } else {
+        printf("It's just the release candidate #%i\n", my_description.release_candidate_number);
+    }
+    if (my_description.is_dirty) {
+        printf("There are uncommited changes\n");
+    } else {
+        printf("Working tree clean\n");
+    }
+    
     //Testing strings
+    printf(SECTION_SEPARATOR);
     char buffer[100];
     my_strcpy(buffer, "Testing");
     printf("(%s)\n", buffer);
@@ -15,6 +41,7 @@ int main(void)
     printf("(%i)\n", my_strcmp("abcx", "abcd"));
 
     //Testing math
+    printf(SECTION_SEPARATOR);
     printf("5! = %lld\n", my_factorial(5));
     printf("0! = %lld\n", my_factorial(0));
     printf("1! = %lld\n", my_factorial(1));
